92be0afa18952755a227a2a01e7ee41278aea819
Matheus Alves <matheuseejam@gmail.com>
2023-06-03 14:14:11 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    TODO navegação
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
